aa91b05278d0e370462ef8fd1c31bbbc2c8c5923
Erick Contreras <er.contreras@hotmail.com>
2023-11-05 19:34:52 -0600

    Add routing

--- lib/peacer.rb
+++ lib/peacer.rb
@@ -2,12 +2,35 @@
 
 require_relative "peacer/version"
 # require 'peacer/array'
+require 'peacer/routing'
 
 module Peacer
   class Application
     def call(env)
-      `echo debug > debug.txt`
-      [200, { "Content-Type" => "text/html" }, ["Hello from Ruby on Peacer!"]]
+      if env["PATH_INFO"] == "/favicon.ico"
+        return [404, { "Content-Type" => "text/html" }, []]
+      end
+
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      begin
+        @text = controller.send(act)
+      rescue Exception => e
+        [500, { "Content-Type" => "text/html" }, ["Something went wrong!"]]
+      end
+      # `echo debug > debug.txt`
+
+      [200, { "Content-Type" => "text/html" }, [@text]]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
